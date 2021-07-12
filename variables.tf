996c9f7fef2d36732b5ada03345ad5676dd15fa2
hegdesandesh225 <sandesh@logiq.ai>
2021-07-12 20:10:45 +0530

    Terraform script to spn up logiq, one click

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "bucket" {
+    type = string
+   default = "logiq-test-poc"
+}
+
+variable "zone" {
+    type=string
+    default="us-central1-a"
+  
+}
+
+variable "project_id" {
+    type=string
+    default="gcp-customer-1"
+  
+}
+
+variable "region"{
+    type=string
+    default = "us-central1"
+}
+
+variable "cred"{
+    type=string
+    default="terraform-gcp1.json"
+
+}
